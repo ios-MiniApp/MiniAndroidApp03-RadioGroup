9ef51507bd60581ad9a0fa7668d5b30ea50d23e0
Kota <maedakoutaa@gmail.com>
2022-09-19 10:07:21 +0900

    commit

---
+++ app/src/main/java/com/example/miniapp03_spinner/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.miniapp03_spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
